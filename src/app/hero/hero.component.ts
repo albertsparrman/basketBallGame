271192535167bffd1b89aef8585df21d35208f0b
albertsparrman <albert.sparrman@gmail.com>
2024-07-12 15:59:40 +0200

    läskig transition

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { setTimeout } from 'node:timers/promises';
 
 @Component({
   selector: 'app-hero',
@@ -9,5 +10,30 @@ import { CommonModule } from '@angular/common';
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
+  currentGamemode: string = 'ALL-STARS'
+  newGamemode: string = 'ALL-STARS'
+  changingGamemode: boolean = false
+
+  @ViewChild('dropdown') dropdown: ElementRef | undefined
+
+  changeGamemode(selectedGamemode: string) {
+    this.newGamemode = selectedGamemode
+    
+    if (this.newGamemode != this.currentGamemode) {
+      this.changingGamemode = true
+
+      window.setTimeout(() => {
+        this.delayCurrentGamemode()
+      }, 3000)
+    }
+    if (this.dropdown != undefined) {
+      this.dropdown.nativeElement.checked = false
+    }
+  }
+
+  delayCurrentGamemode() {
+    this.currentGamemode = this.newGamemode
+    this.changingGamemode = false
+  }
 
 }
